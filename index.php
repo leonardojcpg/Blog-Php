ebd4ab8988980517ee0253281288694dd00d7703
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-23 00:10:17 -0300

    feat: Importing fonts

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-    include_once("helpers/url.php")
+include_once("helpers/url.php")
 
 
 ?>
@@ -14,6 +14,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
     <title>Blog</title>
 </head>
 
