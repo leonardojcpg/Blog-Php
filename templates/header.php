5b69a0d81f2f25d47839a4dadf953c7fefb7cd38
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-23 12:18:49 -0300

    feat: Styling header

--- templates/header.php
+++ templates/header.php
@@ -11,7 +11,7 @@ include_once("helpers/url.php")
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/CSS/styles.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet">
@@ -20,15 +20,15 @@ include_once("helpers/url.php")
 
 <body>
     <header>
-        <a href="<= $BASE_URL ?>" id="logo">
-            <img src="<= $BASE_URL ?>" alt="Blog">
+        <a href="<?= $BASE_URL ?>" id="logo">
+            <img src="<?= $BASE_URL ?>/assets/logo.svg" alt="Blog">
         </a>
         <nav>
             <ul id="navbar">
-                <li><a href="<= $BASE_URL ?>">Home</a></li>
-                <li><a href="#">Categorias</a></li>
-                <li><a href="#">Sobre</a></li>
-                <li><a href="<= $BASE_URL ?>contact.php">Contact</a></li>
+                <li><a href="<?= $BASE_URL ?>" class="nav-link">Home</a></li>
+                <li><a href="#" class="nav-link">Categorias</a></li>
+                <li><a href="#" class="nav-link">Sobre</a></li>
+                <li><a href="<?= $BASE_URL ?>contact.php" class="nav-link">Contact</a></li>
             </ul>
         </nav>
     </header>
