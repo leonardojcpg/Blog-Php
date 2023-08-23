a2307198329da629db8cb0dda03bd69060bd7c81
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-23 00:03:56 -0300

    feat: Creating helpers and css file

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+    include_once("helpers/url.php")
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">
+    <title>Blog</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
